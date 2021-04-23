7e8959bfc3601db0b936217d5ab584dc29ee7919
AzureAD\ИльяЕфимов <jose.bti@gmail.com>
2021-04-23 16:59:17 +0300

    update

--- src/views/Partners.vue
+++ src/views/Partners.vue
@@ -2,26 +2,22 @@
   <div>
     <section class="section section_2">
       <div class="container d-flex justify-content-between">
-        <div class="left align-self-center">
+        <div class="left align-self-center col-md-6">
           <div class="row">
-            <h1>Хочешь получить доступ к интересным событиям, заведениям Калиниграда и области?</h1>
+            <h1>Размести свою компанию в приложении Tourist39
+              и о ней узнают все туристы Калиниграда и области</h1>
           </div>
           <div class="row">
             <div class="go_download">
-              Скачивай приложение <span>Tourist39</span>
+              <a href="https://app.data-crm.com" target="_blank">
+                <button type="button" class="btn btn-primary" >Разместить</button>
+              </a>
+
             </div>
           </div>
-          <div class="row buttons_row">
-            <a href="">
-              <img src="../assets/image 3.png">
-            </a>
-            <a href="">
-              <img src="../assets/image 4.png">
-            </a>
-          </div>
         </div>
-        <div class="right">
-          <img src="../assets/Group426.png">
+        <div class="right col-md-6">
+          <img src="../assets/Group425.png" width="600">
         </div>
       </div>
     </section>
@@ -29,8 +25,14 @@
       <section class="body_section_1">
         <div class="container">
           <h2 class="text-center whats_app">
-            Что есть в приложении?
+            Особенности приложения
           </h2>
+          <div class="row d-flex justify-content-center describe">
+            <div class="col-md-7 col-12 text-center text_body">
+              <span>Tourist39</span> - агрегатор организаций сферы услуг Калининграда и калиниградской области,
+              рекламные компании приложения направлены на привлечение туристического потока
+            </div>
+          </div>
           <div class="row d-flex justify-content-center">
             <div class="card d-flex whats_app_card">
               <div class="container-fluid">
@@ -39,6 +41,7 @@
                     <img src="../assets/Group 139.svg" height="52">
                   </div>
                 </div>
+
                 <div class="row">
                   <h4 class="title">
                     Заведения
@@ -232,53 +235,13 @@
                 Идентификация гостя по QR-коду
               </h3>
               <div class="text_body">
-                Нет необходимости иметь карту лояльности. Её заменяет QR-код в приложении.
+                Клиент сканирует QR-код вашей организации и может подписаться на неё, получать уведомления. При
+                посещении, персонал сканирует индивидуальный QR- код клиента, тем самым идентифицирует его.
+                Дисконтные карты больше не нужны.
               </div>
             </div>
             <div class="col-md-7">
               <img src="../assets/Group 433.png">
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="body_section_5">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-3 offset-1 card">
-              <div class="mega_text">
-                1
-              </div>
-              <h3>
-                Найдите QR-код: свой или в заведении
-              </h3>
-              <div class="text_body">
-                Ваш уникальный QR-код находится во вкладке «Посещение». Код заведения либо на входе либо за
-                cтоликом.
-              </div>
-            </div>
-            <div class="col-md-3 offset-1 card">
-              <div class="mega_text">
-                2
-              </div>
-              <h3>
-                Отсканируйте его или покажите свой
-              </h3>
-              <div class="text_body">
-                Наведите камеру на QR-код, чтобы отсканировать его. Так же можно показать или назвать официанту
-                свой.
-              </div>
-            </div>
-            <div class="col-md-3 offset-1 card">
-              <div class="mega_text">
-                3
-              </div>
-              <h3>
-                Браво! Вы идентифицированы
-              </h3>
-              <div class="text_body">
-                Наведите камеру на QR-код, чтобы отсканировать его. Так же можно показать или назвать официанту
-                свой.
-              </div>
             </div>
           </div>
         </div>
@@ -337,16 +300,13 @@
                 <img src="../assets/Tourist39.png" height="25">
               </div>
               <div class="row">
-                <h4>Устанавливай приложение сейчас и получи бонусы за регистрацию</h4>
+                <h4>Опробуйте инновационный инструмент привлечения клиентов и повышения их лояльности</h4>
               </div>
             </div>
             <div class="col-4 d-flex align-items-center justify-content-end">
               <div class="row buttons_row">
-                <a href="">
-                  <img src="../assets/image 3.png" height="53">
-                </a>
-                <a href="">
-                  <img src="../assets/image 4.png" height="55">
+                <a href="https://app.data-crm.com" target="_blank">
+                  <button type="button" class="btn btn-primary" >Опробовать</button>
                 </a>
               </div>
             </div>
@@ -383,11 +343,204 @@
         </div>
       </div>
     </section>
+    <section class="section section_5 how_much">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="text-center">Сколько стоит размещение в приложении?</h2>
+          </div>
+
+        </div>
+
+        <div class="row d-flex align-items-center">
+          <div class="col-md-7 col-12">
+            <div class="very_big_text">
+              НИСКОЛЬКО
+            </div>
+          </div>
+          <div class="col-md-5 col-12">
+            <div class="text_body">
+              Размещение в приложении Tourist39 бесплатное.
+              Вы платите только за привлечение новых клиентов в Вашу организацию
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <h2 class="text-center">
+              Как стать партнёром?
+            </h2>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 offset-1 card">
+            <div class="mega_text">
+              1
+            </div>
+            <h3>
+              Оставить заявку
+            </h3>
+            <div class="text_body">
+              На этом сайте или по телефону. Менеджер в течение часа свяжется с вами для обсуждения деталей
+            </div>
+          </div>
+          <div class="col-md-3 offset-1 card">
+            <div class="mega_text">
+              2
+            </div>
+            <h3>
+              Заполнить анкету
+            </h3>
+            <div class="text_body">
+              Вам пришлют анкету, в которой необходимо указать данные организации. Они появятся в приложении
+            </div>
+          </div>
+          <div class="col-md-3 offset-1 card">
+            <div class="mega_text">
+              3
+            </div>
+            <h3>
+              Ожидать появления
+            </h3>
+            <div class="text_body">
+              В течение 2 рабочих дней ваше заведение/событие появится в системе
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 d-flex justify-content-center">
+            <a href="https://data-crm.com" target="_blank">
+              <button type="button" class="btn btn-primary">Оставить заявку</button>
+            </a>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-7 mx-auto">
+            <div class="card datacrm_card">
+              <div class="wrap d-flex align-items-center">
+                <img src="../assets/DataCrmLogo.svg">
+                <div class="text_body">
+                  Приложение является частью системы Data CRM и существенно расширяет функционал по работе с гостями и
+                  акциями
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <section class="section section_6 about_data_crm">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="text-center">Особенности Data <span>CRM</span></h2>
+          </div>
+        </div>
+        <div class="row d-flex align-items-center">
+          <div class="col-md-7 col-12">
+            <img src="../assets/_.png" width="600">
+          </div>
+          <div class="col-md-5">
+            <h3>
+              Инновационная работа со столами
+            </h3>
+            <div class="text_body">
+              Все необходимые данные по столам отображаются на одном экране
+            </div>
+          </div>
+        </div>
+        <div class="row d-flex align-items-center">
+          <div class="col-md-5">
+            <h3>
+              Мобильное приложение для персонала
+            </h3>
+            <div class="text_body">
+              Ускоряет и упрощает работу с клиентами. Синхронизируется с десктопной версией
+            </div>
+          </div>
+          <div class="col-md-7 col-12">
+            <div class="wrap d-flex">
+              <img src="../assets/mobile_tarify.png" class="mt-5">
+              <img src="../assets/tables.png" class="mb-5">
+            </div>
+
+          </div>
+
+        </div>
+        <div class="row d-flex align-items-center">
+          <div class="col-md-7 col-12">
+            <img src="../assets/infopanel_desktop.png" width="600">
+          </div>
+          <div class="col-md-5">
+            <h3>
+              Маркетинговые инструменты
+            </h3>
+            <div class="text_body">
+              Создание акций, ABC и RFM анализ, отслеживание эффективности рекламных кампаний
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-7">
+            <div class="text_body text-center primary-text">
+              Система умеет много чего ещё и умеет это хорошо. Все её функци доступны сразу в рамках тарифа, без доплат.
+              Подробная информация на сайте. Есть бесплатный пробный период.
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <a href="https://app.data-crm.com" target="_blank">
+              <button type="button" class="btn btn-primary">Перейти на сайт Data CRM</button>
+            </a>
+
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section section_7 about_data_crm">
+      <div class="row">
+        <div class="col-12">
+          <h2 class="text-center">
+            Часто задаваемые вопросы
+          </h2>
+        </div>
+      </div>
+      <div class="dropdown_element">
+
+      </div>
+    </section>
+    <div class="section_8">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="../assets/undraw_interview_rmcf.svg">
+          </div>
+          <div class="col-md-6 right d-flex align-items-center">
+            <div class="wrap">
+              <h3>Самое время для новых клиентов</h3>
+              <div class="text_body">
+                Оставьте контакты, наш менеджер поможет разместить вашу организацию в приложении. Дайте новым клиентам
+                шанс узнать о вас!
+              </div>
+              <a href="https://app.data-crm.com" target="_blank">
+                <button type="button" class="btn btn-primary">Разместить организацию</button>
+              </a>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Home'
+  name: 'Partners'
 }
 </script>
 <style src="../styles/main.sass" lang="sass">
